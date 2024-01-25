c6435e20ed1baa5ac38717e9393fbb98b62ceeab
= <JoshuaBuchanan13@gmail.com>
2024-01-24 17:09:11 -0700

    More tests

--- location/test/controller/test_address_controller.py
+++ location/test/controller/test_address_controller.py
@@ -4,11 +4,12 @@ import asyncio
 from unittest.mock import patch
 from app.util.redis_manager import RedisManager
 from app.store.address_store import AddressStore
-from app.model.address import CreateAddressRequest, to_address
+from app.model.address import CreateAddressRequest, AddressResponse, to_address, from_dto
 from app.util.google_maps_manager import GoogleMapsManager, GoogleMapsInfo
 from app.controller.address_controller import AddressController
 from app.router.address_router import CreateAddressRequest
 from mockito import when
+
 
 class AddressControllerTest(unittest.IsolatedAsyncioTestCase):
 
@@ -38,23 +39,25 @@ class AddressControllerTest(unittest.IsolatedAsyncioTestCase):
     @patch('app.util.google_maps_manager.GoogleMapsManager')
     @pytest.mark.asyncio
     async def test_post_address(self, redis_manager: RedisManager, address_store: AddressStore,
-                                            google_maps_manager: GoogleMapsManager):
+                                google_maps_manager: GoogleMapsManager):
         impl = AddressController(redis_manager, address_store, google_maps_manager)
         create_address = CreateAddressRequest(
-                address_line_one="Some Street address",
-                city="Denver",
-                state_or_province="CO",
-                zip_or_postal="80234",
-                country_code="US",
-                latitude=1.0,
-                longitude=1.0
+            address_line_one="Some Street address",
+            city="Denver",
+            state_or_province="CO",
+            zip_or_postal="80234",
+            country_code="US",
+            latitude=1.0,
+            longitude=1.0
         )
         google_maps_info = GoogleMapsInfo("place_id_value")
 
         g_map_info = asyncio.Future()
         g_map_info.set_result(google_maps_info)
 
-        when(google_maps_manager).get_google_maps_info(create_address.address(), create_address.city, create_address.state_or_province, create_address.latitude, create_address.longitude).thenReturn(g_map_info)
+        when(google_maps_manager).get_google_maps_info(create_address.address(), create_address.city,
+                                                       create_address.state_or_province, create_address.latitude,
+                                                       create_address.longitude).thenReturn(g_map_info)
 
         address = to_address(create_address, google_maps_info)
 
@@ -66,8 +69,38 @@ class AddressControllerTest(unittest.IsolatedAsyncioTestCase):
         result = await impl.post_address(create_address)
         assert result == 1
 
+    @patch('app.util.redis_manager.RedisManager')
+    @patch('app.store.address_store.AddressStore')
+    @patch('app.util.google_maps_manager.GoogleMapsManager')
+    @pytest.mark.asyncio
+    async def test_find_addresses(self, redis_manager: RedisManager, address_store: AddressStore,
+                                  google_maps_manager: GoogleMapsManager):
+        impl = AddressController(redis_manager, address_store, google_maps_manager)
+        address = AddressResponse(address_id=1)
 
+        address_result = asyncio.Future()
+        address_result.set_result([address])
 
+        when(address_store).find_addresses().thenReturn(address_result)
 
+        result = await impl.find_addresses()
+        assert result == [from_dto(address)]
 
+    @patch('app.util.redis_manager.RedisManager')
+    @patch('app.store.address_store.AddressStore')
+    @patch('app.util.google_maps_manager.GoogleMapsManager')
+    @pytest.mark.asyncio
+    async def test_get_address_by_id_with_cache(self, redis_manager: RedisManager, address_store: AddressStore,
+                                                google_maps_manager: GoogleMapsManager):
+        impl = AddressController(redis_manager, address_store, google_maps_manager)
+        address_id = 1
+        address = AddressResponse()
+        address.address_id = 1
 
+        address_result = asyncio.Future()
+        address_result.set_result(address.json())
+
+        when(redis_manager).get(address_id).thenReturn(address_result)
+
+        result = await impl.get_address_by_id(address_id)
+        assert result == address
